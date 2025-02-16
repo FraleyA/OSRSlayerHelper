b85775a58800b0591787bdcd1ce8fffdb9e6d934
FraleyA <fraley.a@ufl.edu>
2025-02-16 13:50:21 -0500

    scrape_talbe.py stores monster data on a wiki page in dictionaries.

--- .venv/Scripts/main.py
+++ .venv/Scripts/main.py
@@ -1,6 +1,8 @@
 import sqlite3
 import requests
 from bs4 import BeautifulSoup
+from monster_links import monster_links
+from scrape_table import monster_info
 
 # Define pathway to SQL database.
 db_path = 'C:/Users/Austin/PycharmProjects/OSRSlayerHelper/.venv/data/'
@@ -8,42 +10,12 @@ db_path = 'C:/Users/Austin/PycharmProjects/OSRSlayerHelper/.venv/data/'
 # Initialize a database to store relevant info.
 connection = sqlite3.connect(db_path + 'KuradalTasks.db')
 
-# Add header to access OSRS wiki, so that I don't get 403'd.
-header = {'User-Agent': 'Mozilla/5.0'}
+# Scrape links to monster wiki tables off of the desired slayer master's wiki.
+monster_links = monster_links()
 
-# Use requests to access the OSRS wiki.
-r = requests.get('https://oldschool.runescape.wiki/w/Abyssal_demon', headers=header)
+# Iterate through the links to extract monster data.
+for link in monster_links:
+    print(f'Current url: {link}...')
 
-# Check if the request successfully connected.
-if r.status_code != 200:
-    raise RuntimeError(f'Request failed with status code {r.status_code}')
-
-# Parse the OSRS wiki's html source code.
-soup = BeautifulSoup(r.text, 'html.parser')
-
-# Monster name uses 'th' tag in the html wiki page. All monster traits are described as 'data-attr-param'.
-name = soup.find('th', attrs={'data-attr-param': 'name'})
-
-# Make sure the name is found with tag 'th'.
-monster_stats = {}
-if name:
-    monster_stats['name'] = name.text.strip()
-else:
-    print('Warning: Slayer monster name not found.')
-
-# Store monster attribute parameters in a dictionary (wiki html uses 'td' tag for other stats).
-for tag in soup.find_all('td', attrs={'data-attr-param': True}):
-    key = tag.get('data-attr-param')
-    value = tag.text.strip()
-
-    # Check if there is an actual value, not just an empty string; i.e. not an image.
-    # No need for 'dpscalc' either, just tells you to open a dps calculator.
-    if value and key != 'dpscalc':
-        monster_stats[key] = value
-
-print(monster_stats)
-
-
-### --- Now I need to resolve an issue with scraping certain OSRS wiki pages. --- ###
-
-# The issue seems to stem from data being stored in a table as opposed to the tag 'data-attr-param'.
+    monster_data = monster_info(link)
+    print(monster_data, '\n')
--- .venv/Scripts/scrape_table.py
+++ .venv/Scripts/scrape_table.py
@@ -3,99 +3,96 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-# Use requests to access the OSRS wiki.
-base_url = 'https://oldschool.runescape.wiki'
+def monster_info(monster_link):
+    """ Extract monster data from the monster's info table on the wiki. """
 
-# Use header, so the wiki page does not block me. I'm not a bot Jagex, I swear!
-monster_name = 'Adamant_dragon'
-r = requests.get(base_url + '/w/' + monster_name, headers={'User-Agent': 'Mozilla/5.0'})
+    # Use header, so the wiki page does not block me. I'm not a bot Jagex, I swear!
+    r = requests.get(monster_link, headers={'User-Agent': 'Mozilla/5.0'})
 
-# Check if the request successfully connected.
-if r.status_code != 200:
-    raise RuntimeError(f'Request failed with status code {r.status_code}')
+    # Check if the request successfully connected.
+    if r.status_code != 200:
+        raise RuntimeError(f'Request failed with status code {r.status_code}')
 
-# Parse the OSRS wiki's html source code.
-soup = BeautifulSoup(r.text, 'html.parser')
+    # Parse the OSRS wiki's html source code.
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-# Need to find the table class to scrape.
-# table_class1 = 'infobox infobox-switch no-parenthesis-style infobox-monster' # abyssal demon
-# table_class2 = 'infobox no-parenthesis-style infobox-monster' # adamant dragon
+    # Use re.compile() to match the monster data table.
+    info_table = soup.find('table', class_=re.compile('monster'))
 
-info_table = soup.find('table', class_=re.compile('monster'))
+    # Header will store the monster's name data, i.e. 'adamant dragon'.
+    if info_table:
+        header = info_table.find('th', attrs={'class': 'infobox-header'})
+    else: # Return function if no table exists.
+        print('Warning: Info table not found. No monster data found.')
+        return
 
-# Ah yes, the bane of my existence.
-#info_table = soup.find('table', attrs={'class': table_class1})
+    if header:
+        # Define nested dictionary structure based on the wiki table headers.
+        monster_name = header.get_text(strip=True).upper()
+        stat_categories = {
+            monster_name: {},
+            'COMBAT INFO': {},
+            'SLAYER INFO': {},
+            'COMBAT STATS': {},
+            'AGGRESSIVE STATS': {},
+            'MELEE DEFENCE': {},
+            'MAGIC DEFENCE': {},
+            'RANGED DEFENCE': {},
+            'IMMUNITIES': {},
+            'ADVANCED DATA': {}
+        }
+    else:
+        print('Warning: Monster name not found.')
+        return
 
-# header will store the monster's name data, i.e. 'adamant dragon'.
-header = info_table.find('th', attrs={'class': 'infobox-header'})
-if header:
-    # Define nested dictionary structure based on the wiki table headers.
-    monster_name = header.get_text(strip=True).upper()
-    stat_categories = {
-        monster_name: {},
-        'COMBAT INFO': {},
-        'SLAYER INFO': {},
-        'COMBAT STATS': {},
-        'AGGRESSIVE STATS': {},
-        'MELEE DEFENCE': {},
-        'MAGIC DEFENCE': {},
-        'RANGED DEFENCE': {},
-        'IMMUNITIES': {},
-        'ADVANCED DATA': {}
-    }
-else:
-    print('Warning: Monster name not found.')
+    # Iterate through table rows to extract information while tracking the current header.
+    table_rows = info_table.find_all('tr')
+    current_header = monster_name
 
-# Iterate through table rows to extract information while tracking the current header.
-table_rows = info_table.find_all('tr')
-current_header = monster_name
-print(f'Starting header: {current_header}\n')
+    # print(f'Starting header: {current_header}\n')
+    for row in table_rows:
+        # print(row.prettify(), '\n')
 
-for row in table_rows:
-    print(row.prettify(), '\n')
+        # Subheader will store consecutive monster info categories, i.e. 'combat info', 'slayer info', and so on.
+        subheader = row.find('th', class_='infobox-subheader')
 
-    ### INSERT code here to extract header data for released, members, combat level, etc. ###
+        # If we encounter a new subheader, i.e. 'combat info', update current_header.
+        if subheader:
+            current_header = subheader.get_text(strip=True).upper()
+            # print(f'Current subheader is {current_header}...\n')
 
-    # subheader will store consecutive monster info categories, i.e. 'combat info', 'slayer info', and so on.
-    subheader = row.find('th', class_='infobox-subheader')
+        # Store data for each attribute under the current subheader.
+        for header in stat_categories:
+            if header == current_header:
+                attribute = row.find('th')
+                data = row.find('td')
+                if attribute:
+                    attribute_text = attribute.get_text(strip=True).lower()
+                if data:
+                    data_text = data.get_text(strip=True).lower()
 
-    # If we encounter a new subheader, i.e. 'combat info', update current_header.
-    if subheader:
-        current_header = subheader.get_text(strip=True).upper()
-        print(f'Current subheader is {current_header}...\n')
+                if attribute and data:
+                    attribute_text = attribute.get_text(strip=True).lower()
 
-    # Deal with each field accordingly.
-    for header in stat_categories:
-        if header == current_header:
-            attribute = row.find('th')
-            data = row.find('td')
-            if attribute:
-                attribute_text = attribute.get_text(strip=True).lower()
-                print(attribute_text)
-            if data:
-                data_text = data.get_text(strip=True).lower()
-                print(data_text)
+                    # Extract data text while making sure the formatting stays in tact.
+                    data_text = data.get_text('\n', strip=True).lower().replace('\n', ' ')
+                    data_text = re.sub(r'\(\s*(.*?)\s*\)', r'(\1)',
+                                       data_text)  # i.e. '...( dragonfire )' --> '...(dragonfire)'
+                    data_text = re.sub(r'\s*,\s*', r', ',
+                                       data_text)  # i.e. 'attack , stength , defence' --> 'attack, strength, defence'
 
-            if attribute and data:
-                attribute_text = attribute.get_text(strip=True).lower()
-                # Extract text while preserving spaces for newlines
-                data_text = data.get_text('\n', strip=True).lower().replace('\n', ' ')
+                    # Filter out empty strings, i.e. 'assigned by': ''.
+                    if data_text:
+                        stat_categories[current_header][attribute_text] = data_text
 
-                # Filter out empty strings, i.e. 'assigned by': ''.
-                if data_text:
-                    stat_categories[current_header][attribute_text] = data_text
+        combat_skills = row.find_all('a', attrs={'title': True})
+        combat_stats = row.find_all('td', attrs={'class': 'infobox-nested'})
 
-    combat_skills = row.find_all('a', attrs={'title': True})
-    combat_stats = row.find_all('td', attrs={'class': 'infobox-nested'})
+        if combat_skills:
+            combat_skills_text = [skill['title'].strip().lower() for skill in combat_skills]
+        if combat_stats:
+            combat_stats_text = [stat.get_text(strip=True).lower() for stat in combat_stats]
+            for i in range(len(combat_skills_text)):
+                stat_categories[current_header][combat_skills_text[i]] = combat_stats_text[i]
 
-    if combat_skills:
-        combat_skills_text = [skill['title'].strip().lower() for skill in combat_skills]
-        print(combat_skills_text)
-    if combat_stats:
-        combat_stats_text = [stat.get_text(strip=True).lower() for stat in combat_stats]
-        for i in range(len(combat_skills_text)):
-            stat_categories[current_header][combat_skills_text[i]] = combat_stats_text[i]
-
-for category in stat_categories:
-    print(category)
-    print(stat_categories[category], '\n')
+    return stat_categories
